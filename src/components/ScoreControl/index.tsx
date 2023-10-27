fd3c90fafb8b9aba222dbc25522ee222cb65e95f
Gabriel Bursi <gabriellbursii@gmail.com>
2023-10-27 20:15:00 -0300

    implementando teste para ScoreControl

--- src/components/ScoreControl/index.tsx
+++ src/components/ScoreControl/index.tsx
@@ -15,31 +15,45 @@ export const ScoreControl = ({ score }: ScoreControlProps) => {
 	const hasClickedDecrement = useRef(false)
 
 	const incrementScore = () => {
-		if (!hasClickedIncrement.current) {
+		if (!hasClickedIncrement.current && hasClickedDecrement.current) {
+			setScoreNumber(old => old + 2);
+			hasClickedIncrement.current = true
+			hasClickedDecrement.current = false
+		} else if (!hasClickedIncrement.current) {
 			setScoreNumber(old => old + 1);
 			hasClickedIncrement.current = true
 			hasClickedDecrement.current = false
+		} else {
+			setScoreNumber(score)
+			hasClickedIncrement.current = false
 		}
 	}
 
 	const decrementScore = () => {
-		if (!hasClickedDecrement.current && scoreNumber > 0) {
+		if (!hasClickedDecrement.current && hasClickedIncrement.current && scoreNumber > 0) {
+			setScoreNumber(old => old - 2);
+			hasClickedDecrement.current = true
+			hasClickedIncrement.current = false
+		} else if (!hasClickedDecrement.current) {
 			setScoreNumber(old => old - 1);
 			hasClickedDecrement.current = true
 			hasClickedIncrement.current = false
+		} else {
+			setScoreNumber(score)
+			hasClickedDecrement.current = false
 		}
 	}
 
 	return (
 		<S.ScoreControl>
-			<S.ButtonScore onClick={incrementScore}>
-				<AiOutlinePlus size={20} color={hasClickedIncrement.current ? theme.colors.green : lighten(0.2, theme.colors.primary)} />
+			<S.ButtonScore onClick={incrementScore} aria-label="Incrementar Pontuação">
+				<AiOutlinePlus data-testid="icone-incrementar" size={20} color={hasClickedIncrement.current ? theme.colors.green : lighten(0.2, theme.colors.primary)} />
 			</S.ButtonScore>
 			<S.ScoreNumber>
 				{scoreNumber}
 			</S.ScoreNumber>
-			<S.ButtonScore onClick={decrementScore} disabled={scoreNumber === 0}>
-				<AiOutlineMinus size={20} color={hasClickedDecrement.current ? theme.colors.red : lighten(0.2, theme.colors.primary)} />
+			<S.ButtonScore onClick={decrementScore} disabled={scoreNumber === 0} aria-label="Decrementar Pontuação">
+				<AiOutlineMinus data-testid="icone-decrementar" size={20} color={hasClickedDecrement.current ? theme.colors.red : lighten(0.2, theme.colors.primary)} />
 			</S.ButtonScore>
 		</S.ScoreControl>
 	)
--- src/components/ScoreControl/test.tsx
+++ src/components/ScoreControl/test.tsx
@@ -1,11 +1,30 @@
 import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { render } from '../../utils'
 import { ScoreControl } from '.'
 
+const score = 10
+
 describe('<ScoreControl/>', () => {
     it('should render', () => {
-        render(<ScoreControl/>)
+        render(<ScoreControl score={score}/>)
 
-        expect(screen.getByRole('heading', { name: /ScoreControl/i })).toBeInTheDocument()
+		expect(screen.getByText(score)).toBeInTheDocument()
+		expect(screen.getAllByRole('button')).toHaveLength(2)
     })
+
+	it('Increment and decrement score', async () => {
+		render(<ScoreControl score={score}/>)
+
+		userEvent.setup()
+
+		const scoreElement = screen.getByText(score)
+		const botaoIncrementar = screen.getByRole('button', { name: /Incrementar Pontuação/i });
+		const botaoDecrementar = screen.getByRole('button', { name: /Decrementar Pontuação/i });
+
+		await userEvent.click(botaoIncrementar)
+		expect(scoreElement).toHaveTextContent(`${score + 1}`)
+		await userEvent.click(botaoDecrementar)
+		expect(scoreElement).toHaveTextContent(`${score - 1}`)
+	})
 })
