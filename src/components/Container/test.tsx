d4cd45374f985e62c3fca893c5d5af1ddd385129
Gabriel Bursi <gabriellbursii@gmail.com>
2023-10-27 20:53:26 -0300

    implementando teste Container

--- src/components/Container/test.tsx
+++ src/components/Container/test.tsx
@@ -1,36 +1,18 @@
-import { render } from '@testing-library/react'
+import { render } from '../../utils'
 import { Container } from '.'
-import { theme } from '@/styles'
+import { theme } from '../../styles'
 
 describe('<Container/>', () => {
-    it('should render the heading', () => {
+    it('should render', () => {
         const { container } = render(
             <Container>
-                <span>Won Games</span>
+                <span>Container</span>
             </Container>
         )
 
         expect(container.firstChild).toHaveStyleRule(
             'max-width',
             theme.grid.container
-        )
-
-        expect(container.firstChild).toMatchInlineSnapshot(`
-            .c0 {
-                max-width: 130rem;
-                margin-left: auto;
-                margin-right: auto;
-                padding-left: calc(3.2rem / 2);
-                padding-right: calc(3.2rem / 2);
-            }
-            <div
-                class="c0"
-            >
-                <span>
-                Won Games
-                </span>
-            </div>
-            `
         )
     })
 })
