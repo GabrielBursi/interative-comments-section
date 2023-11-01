89a500a09ea1ec97e3f3f8888f9a7df8c670774c
Gabriel Bursi <gabriellbursii@gmail.com>
2023-11-01 16:03:24 -0300

    testes UserInfo

--- src/components/UserInfo/test.tsx
+++ src/components/UserInfo/test.tsx
@@ -1,42 +1,84 @@
 import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event';
 import { formatDistanceToNow } from 'date-fns';
+
 import { render } from '../../utils'
 import { UserInfo } from '.'
-import { User } from '@/types'
 import DefaultAvatar from '../../../public/images/avatars/default.png'
+import { UserInfoProps } from './types';
 
-const user: User = {
-	username: 'blondehot',
-	avatar: DefaultAvatar.src
+const createdAt = '2023-10-25T09:15:00Z';
+
+const props: UserInfoProps = {
+	user: {
+		username: 'blondehot',
+		avatar: DefaultAvatar.src
+	},
+	isOwn: false,
+	createdAt,
+	id: 1,
+	onEdit: () => ''
 }
 
-const createAt = '2023-10-25T09:15:00Z';
-const formattedDate = formatDistanceToNow(new Date(createAt), { addSuffix: true });
+const formattedDate = formatDistanceToNow(new Date(createdAt), { addSuffix: true });
 
 describe('<UserInfo/>', () => {
-    it('should render', () => {
-		render(<UserInfo createdAt={createAt} user={{...user}} isOwn/>)
+    it('should render normal variant', () => {
+		render(<UserInfo {...props}/>)
 
 		expect(screen.getByRole('img')).toBeInTheDocument()
-		expect(screen.getByText(`${user.username}`)).toBeInTheDocument()
-		expect(screen.getByText(/você/i)).toBeInTheDocument()
+		expect(screen.getByText(`${props.user.username}`)).toBeInTheDocument()
 		expect(screen.getByText(formattedDate)).toBeInTheDocument()
-		expect(screen.getByRole('button', { name: /Excluir/i })).toBeInTheDocument()
-		expect(screen.getByRole('button', { name: /Editar/i })).toBeInTheDocument()
+		expect(screen.queryByRole('button', { name: /Responder/i })).toBeInTheDocument()
+    })
+
+	it('should render isOwn variant', () => {
+		render(<UserInfo {...props} isOwn/>)
 
 		expect(screen.queryByRole('button', { name: /Responder/i })).not.toBeInTheDocument()
+		expect(screen.getByText(/você/i)).toBeInTheDocument()
+		expect(screen.getByRole('button', { name: /Excluir/i })).toBeInTheDocument()
+		expect(screen.getByRole('button', { name: /Editar/i })).toBeInTheDocument()
     })
 
-	it('should render without isOwn prop', () => {
-		render(<UserInfo createdAt={createAt} user={{...user}} isOwn={false}/>)
+	it('should open and close modal delete', async () => {
+		render(<UserInfo {...props} isOwn />)
 
-		expect(screen.getByRole('img')).toBeInTheDocument()
-		expect(screen.getByText(`${user.username}`)).toBeInTheDocument()
-		expect(screen.getByText(formattedDate)).toBeInTheDocument()
-		expect(screen.getByRole('button', { name: /Responder/i })).toBeInTheDocument()
+		userEvent.setup()
 
-		expect(screen.queryByRole('button', { name: /Excluir/i })).not.toBeInTheDocument()
-		expect(screen.queryByRole('button', { name: /Editar/i })).not.toBeInTheDocument()
-		expect(screen.queryByText(/você/i)).not.toBeInTheDocument()
-    })
+		const btnDelete = screen.getByRole('button', { name: /Excluir/i })
+		await userEvent.click(btnDelete)
+
+		const overlay = screen.getByTestId('overlay')
+
+		expect(overlay.getAttribute('aria-hidden')).toBe('false')
+		expect(overlay).toHaveStyle('opacity: 1')
+		expect(overlay).toHaveStyle('display: flex')
+
+		const btnClose = screen.getByRole('button', { name: /não, cancelar/i })
+
+		await userEvent.click(btnClose)
+
+		expect(overlay.getAttribute('aria-hidden')).toBe('true')
+		expect(overlay).toHaveStyle('opacity: 0')
+		expect(overlay).toHaveStyle('display: none')
+	})
+	
+	it('should confirm modal delete', async () => {
+		render(<UserInfo {...props} isOwn />)
+
+		userEvent.setup()
+		const overlay = screen.getByTestId('overlay')
+
+		const btnDelete = screen.getByRole('button', { name: /Excluir/i })
+		await userEvent.click(btnDelete)
+
+		const btnConfirm = screen.getByRole('button', { name: /sim, excluir/i })
+
+		await userEvent.click(btnConfirm)
+
+		expect(overlay.getAttribute('aria-hidden')).toBe('true')
+		expect(overlay).toHaveStyle('opacity: 0')
+		expect(overlay).toHaveStyle('display: none')
+	})
 })
